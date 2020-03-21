d16efcf32dcd70355e6a899eef75cf1d1be4a38b
listen <listenzz@163.com>
2020-03-21 11:08:35 +0800

    fix click back issue

--- HBDNavigationBar/Classes/HBDNavigationController.m
+++ HBDNavigationBar/Classes/HBDNavigationController.m
@@ -454,6 +454,9 @@ UIColor* blendColor(UIColor *from, UIColor *to, float percent) {
 }
 
 - (void)fixClickBackIssue {
+    if (@available(iOS 13.0, *)) {
+        return;
+    }
     if (@available(iOS 11.0, *)){
         // fix：ios 11，12，当前后两个页面的 barStyle 不一样时，点击返回按钮返回，前一个页面的标题颜色响应迟缓或不响应
         id<UIViewControllerTransitionCoordinator> coordinator = self.transitionCoordinator;
